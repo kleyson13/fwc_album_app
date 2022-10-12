1bc1aa5bba0c3ac208f166986772d23b037db7e0
Kleyson Mariano <kleysons913@gmail.com>
2022-10-12 16:36:34 -0300

    Identificação Fake Usuário

--- lib/app/fwc_album_app.dart
+++ lib/app/fwc_album_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fwc_album_app/app/core/theme/theme_config.dart';
 import 'package:fwc_album_app/app/pages/auth/login/login_page.dart';
 import 'package:fwc_album_app/app/pages/home/home_page.dart';
-import 'package:fwc_album_app/app/pages/splash/splash_page.dart';
+import 'package:fwc_album_app/app/pages/splash/splash_route.dart';
 
 class FwcAlbumApp extends StatelessWidget {
   const FwcAlbumApp({super.key});
@@ -14,7 +14,7 @@ class FwcAlbumApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeConfig.theme,
       routes: {
-        '/': (_) => const SplashPage(),
+        '/': (_) => const SplashRoute(),
         '/auth/login': (_) => const LoginPage(),
         '/home': (_) => const HomePage(),
       },
