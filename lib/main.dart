7907e77ceb47dffc906cb1dce11545b867e01e29
Kleyson Mariano <kleysons913@gmail.com>
2022-10-12 16:50:03 -0300

    Configuração do environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album_app/app/fwc_album_app.dart';
 
-void main() {
+import 'app/core/config/env/env.dart';
+
+Future<void> main() async {
+  await Env.i.load();
   runApp(const FwcAlbumApp());
 }
