1a246e21434919db2329e63d2385a7a5feead23a
Kleyson Mariano <kleysons913@gmail.com>
2022-10-15 14:19:49 -0300

    Limitando o contexto global

--- lib/app/core/rest/interceptors/auth_interceptor.dart
+++ lib/app/core/rest/interceptors/auth_interceptor.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:fwc_album_app/app/core/ui/global/global_context.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthInterceptor extends Interceptor {
@@ -10,5 +12,14 @@ class AuthInterceptor extends Interceptor {
     options.headers['Authorization'] = 'Bearer $accessToken';
 
     handler.next(options);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    if (err.response?.statusCode == 401) {
+      Injector.get<GlobalContext>().loginExpire();
+    } else {
+      handler.next(err);
+    }
   }
 }
