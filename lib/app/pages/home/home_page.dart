82f55a442dc2e99ed9ca34844d44be3bf919d145
Kleyson Mariano <kleysons913@gmail.com>
2022-10-15 17:01:31 -0300

    Implementação Home_Page

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/pages/home/presenter/home_presenter.dart';
+import 'package:fwc_album_app/app/pages/home/view/home_view_impl.dart';
 import 'package:fwc_album_app/app/pages/home/widgets/status_tile.dart';
 import 'package:fwc_album_app/app/pages/home/widgets/sticker_percent_widget.dart';
-import '../../core/rest/custom_dio.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  final HomePresenter presenter;
 
+  const HomePage({super.key, required this.presenter});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends HomeViewImpl {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +27,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: context.colors.primary,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => widget.presenter.logout(),
             icon: const Icon(Icons.logout),
             color: Colors.white,
           ),
@@ -49,14 +56,14 @@ class HomePage extends StatelessWidget {
                             fit: BoxFit.cover,
                           ),
                         ),
-                        const StickerPercentWidget(
-                          percent: 65,
+                        StickerPercentWidget(
+                          percent: user?.totalCompletePercent ?? 0,
                         ),
                         const SizedBox(
                           height: 20,
                         ),
                         Text(
-                          '45 figurinhas',
+                          '${user?.totalStickers ?? 0} figurinhas',
                           style: context.textStyles.titleWhite,
                         ),
                         const SizedBox(
@@ -65,7 +72,7 @@ class HomePage extends StatelessWidget {
                         StatusTile(
                           icon: Image.asset('assets/images/all_icon.png'),
                           label: 'Todas',
-                          value: 34,
+                          value: user?.totalAlbum ?? 0,
                         ),
                         const SizedBox(
                           height: 20,
@@ -73,7 +80,7 @@ class HomePage extends StatelessWidget {
                         StatusTile(
                           icon: Image.asset('assets/images/missing_icon.png'),
                           label: 'Faltando',
-                          value: 500,
+                          value: user?.totalComplete ?? 0,
                         ),
                         const SizedBox(
                           height: 20,
@@ -81,13 +88,15 @@ class HomePage extends StatelessWidget {
                         StatusTile(
                           icon: Image.asset('assets/images/repeated_icon.png'),
                           label: 'Repetidas',
-                          value: 30,
+                          value: user?.totalDuplicates ?? 0,
                         ),
                         const SizedBox(
                           height: 20,
                         ),
                         Button(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/my-stickers');
+                          },
                           width: MediaQuery.of(context).size.width * .9,
                           outline: true,
                           style: context.buttonStyles.yellowOutlineButton,
